7a259a2a427e964728fd57a1851b78d172df35b0
agsdo <agsdeoliveira16@gmail.com>
2022-04-28 23:40:12 -0300

    Testando Variáveis e print

---
+++ Aula1.py
@@ -0,0 +1,9 @@
+print ('Duas pessoas se amam. São eles:')
+
+idade = 30
+Nome_usuario = 'Alexsandro'
+print (Nome_usuario, ', que tem', idade, "anos!")
+
+idade = 26
+Nome_usuario = 'Letícia'
+print (Nome_usuario, ', que tem', idade, "anos!")
