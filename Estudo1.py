a50acac773131824496e23b2c1f5f036a5fda7bc
agsdo <agsdeoliveira16@gmail.com>
2022-05-01 14:57:28 -0300

    calculo e conversão em f-strings

--- Estudo1.py
+++ Estudo1.py
@@ -1,3 +1,4 @@
 nome_usuario = input('Digite seu nome:')
-idade = input('Digite sua idade:')
-print(f"Nome do Usuário:{nome_usuario}. {idade} anos.")
+ano_nascimento = int(input('Digite o ano do seu nascimento:'))
+
+print(f"Nome do Usuário:{nome_usuario} {2022-ano_nascimento} anos.")
