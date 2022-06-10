a07c8cd38b60eaf760b17594efe16e2082ae25ca
agsdo <agsdeoliveira16@gmail.com>
2022-06-09 21:28:38 -0300

    atualização

--- Estudo1.py
+++ Estudo1.py
@@ -1,4 +1,4 @@
 nome_usuario = input('Digite seu nome:')
-ano_nascimento = int(input('Digite o ano do seu nascimento:'))
+ano_nascimento = input('Digite o ano do seu nascimento:')
 
-print(f"Nome do Usuário:{nome_usuario} {2022-ano_nascimento} anos.")
+print(f"Nome do Usuário:{nome_usuario} {2022-int(ano_nascimento)} anos.")
